3677d7b718eee064570a92d88c960cd1f62ef50a
Erwin Antepuesto <66218784+sciyon@users.noreply.github.com>
2024-07-21 22:48:50 +0800

    middleware 34.49

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
+app.use(express.json())
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 //where to look for static paths
 
 app.use('/', require('./routes/root'))
 
 app.all('*', (req, res ) => {
   res.status(404)
-  if (req.accepts('html')) {
+  if ( req.accepts('html') ) {
     res.sendFile(path.join(__dirname, 'views', '404.html'))
 
   }else if ( req.accepts('json') ){
